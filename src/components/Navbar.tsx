7d49f7e8c875cca451c3d42a76753c06643f6c05
Seble Mengistu <seblemen94@gmail.com>
2026-09-09 11:50:15 -0700

    Add Home back to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,6 +22,7 @@ const Navbar = () => {
   ];
 
   const navLinks = [
+    { name: "Home", path: "/" },
     { name: "Services", path: "/services" },
     { name: "About", path: "/about" },
     { name: "Solutions", path: "/solutions" },
